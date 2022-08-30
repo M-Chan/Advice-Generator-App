3089e7602a68ba6f3dd40f2c7cf1982a32b6246d
M-Chan <mantingchan2@gmail.com>
2022-08-30 20:07:41 +0100

    fetched API and displays the data

---
+++ app/app.js
@@ -0,0 +1,32 @@
+//declaring varibles...
+let adviceNoHolder = document.getElementById("adviceID");
+let adviceHolder = document.getElementById("quote");
+
+let adviceNo;
+let advice;
+
+function generateAdvice() {
+    // fetch API...
+    fetch("https://api.adviceslip.com/advice")
+
+    .then((response)=>{
+        return response.json();
+    })
+
+    .then((data)=>{
+    console.log(data); // displays one set of data in the console
+
+    adviceNo = data.slip.id;
+    console.log("id = ", adviceNo)
+    adviceNoHolder.innerText = adviceNo;
+
+    advice = data.slip.advice;
+    console.log("advice = ", advice)
+    adviceHolder.innerText = "\"" + advice + "\"";
+    })
+
+    .catch(err => console.log(err));
+}
+
+generateAdvice();
+
